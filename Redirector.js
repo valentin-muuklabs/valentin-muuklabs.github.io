f6418a4abb19bf66add903e9a738de7cbe658eda
unknown <valentin@muuklabs.com>
2019-11-11 10:42:59 -0800

    Instant redirect

--- Redirector.js
+++ Redirector.js
@@ -1,16 +1,8 @@
-var count=5;
-function redirect(){
-  setInterval(countDown,1000);
-}
+function reidrect(){
+  let secret=Math.random();
+  secret="secret="+secret;
+  document.cookie = secret;
+  document.location.href="Target.html?"+secret;
 
-function countDown(){
-  document.getElementById("count").innerText=count;
-  count = count-1;
-  console.log("count "+count);
-  if(count===0){
-    let secret=Math.random();
-    secret="secret="+secret;
-    document.cookie = secret;
-    document.location.href="Target.html?"+secret;
-  }
 }
+redirect();
